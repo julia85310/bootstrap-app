4eb2990c674a883fc616a35b1d8f4b4d3db73d7d
julia85310 <148855799+julia85310@users.noreply.github.com>
2025-02-03 12:16:20 +0100

    listoo

--- src/app/components/ModalExample.js
+++ src/app/components/ModalExample.js
@@ -1,27 +1,31 @@
+'use client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-export default function ModalExample(){
-    return (<>
-        <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Viaja con nosotros
-        </button>
-        <div className="modal" id='exampleModal' tabIndex="-1">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Descarga SkyScanner</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    <p>Vuelos al mejor precio✈️🤑</p>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Visitar web</button>
-                </div>
+export default function ModalExample() {
+    return (
+        <>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Viaja con nosotros
+            </button> 
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Descarga SkyScanner</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <p>Vuelos al mejor precio✈️🤑</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button type="button" className="btn btn-primary">Visitar web</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
